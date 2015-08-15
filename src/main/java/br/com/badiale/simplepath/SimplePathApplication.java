05f290af590128c8db4f778b61742b562db399e9
Lucas Badiale <lcbadial@gmail.com>
2015-08-15 14:20:08 -0300

    Initial commit

---
+++ src/main/java/br/com/badiale/simplepath/SimplePathApplication.java
@@ -0,0 +1,12 @@
+package br.com.badiale.simplepath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePathApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplePathApplication.class, args);
+    }
+}
